a1ac7fbb292a1a31ff4e82d2552af7118ad82956
jorgeATRB <126728231+jorgeATRB@users.noreply.github.com>
2023-03-02 19:45:41 -0400

    Update dinosaur.py

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,14 @@
+#clase Dinasaur, metodo constructor,update,draw pass
+class Dinasour:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+    
+    def draw(self):
+        pass
+#investigar que son Sprites
+#investigar herencia en programacion
+
+#establecer una imagen de perfil a las cuentas de teams
